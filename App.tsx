1e0a17d03ca47a08b71bf59f285d0c5285d4eaaa
Dima Portenko <troublesohard@gmail.com>
2023-04-26 20:21:24 +0300

    template

--- App.tsx
+++ App.tsx
@@ -1,12 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  ListRenderItemInfo,
+  SafeAreaView,
+  StyleSheet,
+} from "react-native";
+import { DataItemType, getData } from "./src/data";
+import { ListItem } from "./src/ListItem";
 
 export default function App() {
+  const data = getData();
+  console.log(data);
+
+  const renderItem = ({ item }: ListRenderItemInfo<DataItemType>) => {
+    return <ListItem item={item} />;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={data}
+        keyExtractor={(item, index) => `${item.title}${index}`}
+        renderItem={renderItem}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +30,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
